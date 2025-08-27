fa4b99540e1ee1e36c2fbdc14873bf74819ed2e3
Sergioalv15 <sergioalv961@gmail.com>
2025-08-27 22:03:25 +0000

    [terminado navbar y jumbotron] falta card por completar

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,10 +9,13 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+import  Home  from "./components/Home";
+import  Navbar  from "./components/Navbar";
+import  Jumbotron  from "./components/Jumbotron";
+import  Card  from "./components/Card";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <Home></Home>
   </React.StrictMode>,
 )
